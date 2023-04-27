b6df48a1afc5a6adb6506f8c389af4942f9262e5
Sandro Sp <38314662+SanJSp@users.noreply.github.com>
2023-04-27 19:15:41 +0200

    Update storage_manager.cpp

--- src/lib/storage/storage_manager.cpp
+++ src/lib/storage/storage_manager.cpp
@@ -5,33 +5,45 @@
 namespace opossum {
 
 StorageManager& StorageManager::get() {
-  return *(new StorageManager());
-  // A really hacky fix to get the tests to run - replace this with your implementation
+  static auto instance = StorageManager{};
+  return instance;
 }
 
 void StorageManager::add_table(const std::string& name, std::shared_ptr<Table> table) {
-  // Implementation goes here
-  Fail("Implementation is missing.");
+  _tables[name] = table;
 }
 
 void StorageManager::drop_table(const std::string& name) {
-  // Implementation goes here
-  Fail("Implementation is missing.");
+  if (_tables[name]) {
+    _tables[name] = nullptr;
+  } else {
+    throw std::logic_error("Table does not exist");
+  }
 }
 
 std::shared_ptr<Table> StorageManager::get_table(const std::string& name) const {
-  // Implementation goes here
-  Fail("Implementation is missing.");
+  if (_tables.at(name)) {
+    return _tables.at(name);
+  } else {
+    throw std::logic_error("Table does not exist");
+  }
 }
 
 bool StorageManager::has_table(const std::string& name) const {
-  // Implementation goes here
-  Fail("Implementation is missing.");
+  if (_tables.contains(name))
+    return true;
+  return false;
 }
 
 std::vector<std::string> StorageManager::table_names() const {
-  // Implementation goes here
-  Fail("Implementation is missing.");
+  auto keys = std::vector<std::string>{};
+  keys.reserve(_tables.size());
+
+  for (auto [key, _] : _tables) {
+    keys.push_back(key);
+  }
+
+  return keys;
 }
 
 void StorageManager::print(std::ostream& out) const {
@@ -40,7 +52,7 @@ void StorageManager::print(std::ostream& out) const {
 }
 
 void StorageManager::reset() {
-  // Implementation goes here
+  _tables = std::unordered_map<std::string, std::shared_ptr<Table>>();
 }
 
 }  // namespace opossum
