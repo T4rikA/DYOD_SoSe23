3c4c19bb8a2c9399f06671190b063627277ee14a
Sandro Speh <sandrospeh@gmail.com>
2023-04-29 08:59:14 +0200

    Refactoring: Protected variables start with underscore

--- src/lib/storage/chunk.cpp
+++ src/lib/storage/chunk.cpp
@@ -6,28 +6,28 @@
 namespace opossum {
 
 void Chunk::add_segment(const std::shared_ptr<AbstractSegment> segment) {
-  segments.push_back(segment);
+  _segments.push_back(segment);
 }
 
 void Chunk::append(const std::vector<AllTypeVariant>& values) {
-  DebugAssert(values.size() == segments.size(), "There must be the same amount of values as in the segment!");
+  DebugAssert(values.size() == _segments.size(), "There must be the same amount of values as in the segment!");
   for(size_t i = 0; i < values.size(); ++i){
-    auto segment = segments[i];
+    auto segment = _segments[i];
     segment->append(values[i]);
   }
 }
 
 std::shared_ptr<AbstractSegment> Chunk::get_segment(const ColumnID column_id) const {
-  return segments[column_id];
+  return _segments[column_id];
 }
 
 ColumnCount Chunk::column_count() const {
-  return ColumnCount(segments.size()); 
+  return ColumnCount(_segments.size());
 }
 
 ChunkOffset Chunk::size() const {
-  if(segments.size()){
-    return segments[0] -> size();
+  if(_segments.size()){
+    return _segments[0] -> size();
   }
   return 0;
 }
