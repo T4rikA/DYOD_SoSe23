6f460afc4d614b22747b1a3d27141a48f86b6419
Tarik Alnawa <talnawa@outlook.de>
2023-05-17 15:20:27 +0200

    Add memory estimate and missing tests

--- src/lib/storage/dictionary_segment.cpp
+++ src/lib/storage/dictionary_segment.cpp
@@ -29,11 +29,12 @@ DictionarySegment<T>::DictionarySegment(const std::shared_ptr<AbstractSegment>& 
 
   auto values = value_segment->values();
   auto value_segment_size = value_segment->size();
+  _segment_nullable = value_segment->is_nullable();
 
   // stores value -> id
   auto unique_values = std::map<T, ValueID>();
 
-  auto last_index = value_segment->is_nullable() ? 1 : 0;
+  auto last_index = _segment_nullable ? 1 : 0;
   for (auto index = size_t{0}; index < value_segment_size; ++index) {
     if (!value_segment->is_null(index)) {
       auto value = values[index];
@@ -80,7 +81,7 @@ T DictionarySegment<T>::get(const ChunkOffset chunk_offset) const {
 
 template <typename T>
 std::optional<T> DictionarySegment<T>::get_typed_value(const ChunkOffset chunk_offset) const {
-  const auto value_id = static_cast<ValueID>(attribute_vector()->get(chunk_offset));
+  const auto value_id = attribute_vector()->get(chunk_offset);
   if (value_id == null_value_id()) {
     return std::nullopt;
   }
@@ -99,13 +100,13 @@ std::shared_ptr<AbstractAttributeVector> DictionarySegment<T>::attribute_vector(
 
 template <typename T>
 ValueID DictionarySegment<T>::null_value_id() const {
-  return static_cast<ValueID>(0);
+  return _segment_nullable ? static_cast<ValueID>(0) : INVALID_VALUE_ID;
 }
 
 template <typename T>
 const T DictionarySegment<T>::value_of_value_id(const ValueID value_id) const {
-  Assert(value_id != null_value_id(), "Can't retrieve value for null value.");
-  return dictionary().at(value_id);
+  Assert(_segment_nullable && value_id != null_value_id(), "Can't retrieve value for null value.");
+  return dictionary().at(value_id - (_segment_nullable ? 1 : 0));
 }
 
 template <typename T>
@@ -148,7 +149,9 @@ ChunkOffset DictionarySegment<T>::size() const {
 
 template <typename T>
 size_t DictionarySegment<T>::estimate_memory_usage() const {
-  return size_t{dictionary().size() * sizeof(uint32_t) /*+ attribute_vector().size() * sizeof (T)*/};
+  auto dict_size = sizeof(T) * dictionary().size();
+  auto att_vec_size = attribute_vector()->width() * attribute_vector()->size();
+  return dict_size + att_vec_size;
 }
 
 EXPLICITLY_INSTANTIATE_DATA_TYPES(DictionarySegment);
